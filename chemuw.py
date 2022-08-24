5480a8df85819bc060ae719f186be03a4594260b
Lauren Knight <lknight2@mail.umw.edu>
2022-08-24 12:12:31 -0400

    add maximum string length of 32

--- chemuw.py
+++ chemuw.py
@@ -10,19 +10,24 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         message = request.get_json()['message']
+        # no huge strings
+        if len(message) > 32:
+            s = s[0:32]
         if message == 's':
-            # reg = {}
-            # reg['value'] = '0xfeedabee'
-            # reg['register'] = random.randint(0, 15)
-            # reg16 = {}
-            # reg16['value'] = '0xFFFFFFFF'
-            # reg16['register'] = 16
-            # register_updates = [reg, reg16]
-            # instruction = {}
-            # instruction['address'] = '0x00abcdef'
-            # instruction['instruction'] = 'test abc def'
-            # instruction_frame = [instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction]
             result = chemu.do(message)
             return jsonify(register_updates=result['registers'], instruction_frame=result['instructions'])
+        return None
     else:
         return render_template('index.html')
+
+# reg = {}
+# reg['value'] = '0xfeedabee'
+# reg['register'] = random.randint(0, 15)
+# reg16 = {}
+# reg16['value'] = '0xFFFFFFFF'
+# reg16['register'] = 16
+# register_updates = [reg, reg16]
+# instruction = {}
+# instruction['address'] = '0x00abcdef'
+# instruction['instruction'] = 'test abc def'
+# instruction_frame = [instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction,instruction]
